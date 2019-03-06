bd0c14183e2521eaa1599ad444876851e990d912
SarinaPhejlada <sarina1116@gmail.com>
2019-03-06 15:20:11 -0500

    Added login verification code

--- app/src/main/java/com/example/bookies/LoginActivity.java
+++ app/src/main/java/com/example/bookies/LoginActivity.java
@@ -9,11 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -22,10 +24,14 @@ import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
     //widgets
-    Button loginButton
-            ,forgotPasswordButton
-            ,createAccountButton;
+    private Button loginButton; //login button
+    private Button forgotPasswordButton; //forgot password button
+    private Button createAccountButton; //create an account button
+    private EditText username; //edittext view for username/email
+    private EditText password; //edittext view for password
 
+    //data base reference
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +42,26 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = (Button) findViewById(R.id.loginBtn);
         forgotPasswordButton = (Button) findViewById(R.id.forgotBtn);
         createAccountButton = (Button) findViewById(R.id.createBtn);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        //connecting to database
+        db = FirebaseFirestore.getInstance();//database instance
 
         //code for login button
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(i);
+                try {
+                    //This method verifies the credentials the user entered
+                    getLogin(String.valueOf(username.getText()), String.valueOf(password.getText()));
+                }
+                catch(Exception e){
+                    Toast.makeText(LoginActivity.this
+                            ,"Invalid username or password"
+                            ,Toast.LENGTH_LONG)
+                            .show();
+                }
             }
         });
         //code for retrieving forgotten button
@@ -63,5 +82,39 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    //retrieves login from database
+    private void getLogin(final String username, final String password){
+        db.collection("account")
+                .document(username)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            try{
+                                DocumentSnapshot document = task.getResult();
+                                if(document.getData().get("password").toString().equals(password)) {
+                                    //passing data to Review Activity
+                                    Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                                    startActivity(i);
+                                }
+                                else{
+                                    Toast.makeText(LoginActivity
+                                            .this,"Incorrect password",Toast
+                                            .LENGTH_LONG)
+                                            .show();
+                                }
+                            }
+                            //in case retrieval fails
+                            catch(Exception e){
+                                Toast.makeText(LoginActivity
+                                        .this,"Username not found",Toast
+                                        .LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+                    }
+                });
+    }
 
 }
