59aae26e959b5682fd0fe4f52d97e5e57673c55f
SarinaPhejlada <sarina1116@gmail.com>
2019-04-02 01:08:15 -0400

    added proper signout

--- app/src/main/java/com/example/bookies/HomeActivity.java
+++ app/src/main/java/com/example/bookies/HomeActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +18,7 @@ public class HomeActivity extends AppCompatActivity {
         Button search = (Button) findViewById(R.id.searchBtn);
         Button history = (Button) findViewById(R.id.historyBtn);
         Button logout = (Button) findViewById(R.id.logoutBtn);
+        mAuth = FirebaseAuth.getInstance();
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,8 +36,15 @@ public class HomeActivity extends AppCompatActivity {
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(i);
+                try{
+                    mAuth.signOut();
+                    Toast.makeText(HomeActivity.this, "Successfully logged out", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+                }
+                catch(Exception e){
+                    Toast.makeText(HomeActivity.this, "Error occurred logging out", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
