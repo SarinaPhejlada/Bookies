d3ee3e5a73c93f33533cae2afe5918673746609a
Kevin <kevinvinhn@gmail.com>
2019-04-08 14:36:44 -0400

    something

---
+++ app/src/main/java/com/example/bookies/SearchHistory.java
@@ -0,0 +1,85 @@
+package com.example.bookies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+
+
+import androidx.annotation.NonNull;
+
+public class SearchHistory extends AppCompatActivity {
+
+    private static String TAG = "SearchHistory";
+    //database reference
+    private FirebaseFirestore db;
+
+    public SearchHistory(FirebaseFirestore db){
+        this.db = db;
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+    }
+    void runAll(){
+        Log.d(TAG, "BEGIN");
+
+        //Run all our methods
+        getDB();
+
+        //Run methods that should fail
+        try{
+            compoundQueriesInvalid();
+        }
+        catch(Exception e){
+            Log.d(TAG, "compoundQueriesInvalid", e);
+        }
+        try{
+            orderAndLimitInvalid();
+        }
+        catch(Exception e){
+            Log.d(TAG, "orderAndLimitInvalid", e);
+        }
+    }
+    public void setup() {
+        // [START get_firestore_instance]
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // [END get_firestore_instance]
+
+        // [START set_firestore_settings]
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+        // [END set_firestore_settings]
+    }
+    private void getDB(FirebaseFirestore db) {
+        db.collection("account")
+                .whereEqualTo("", true)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        }
+                        else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+}
