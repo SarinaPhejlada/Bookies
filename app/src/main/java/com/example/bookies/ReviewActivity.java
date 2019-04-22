e8b3bdcf264dc0343ea1a0a26462681b14856772
SarinaPhejlada <sarina1116@gmail.com>
2019-04-22 15:28:44 -0400

    made review scrollable

--- app/src/main/java/com/example/bookies/ReviewActivity.java
+++ app/src/main/java/com/example/bookies/ReviewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 
 import android.widget.ImageButton;
@@ -67,6 +68,8 @@ public class ReviewActivity extends AppCompatActivity {
 
         //adding book to history table
         addHistory(mAuth.getCurrentUser().getEmail(), book);
+
+        reviewTextView.setMovementMethod(new ScrollingMovementMethod());
 
     }
 
